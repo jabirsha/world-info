970229a546446314cd8051780c56f01bd4580116
jabirshariff12@gmail.com <you@example.com>
2023-08-01 20:21:44 +0530

    A project directory created

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <p className="app_header">Countries Information!!</p>
+    </div>
+  );
+}
+
+export default App;
