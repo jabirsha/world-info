e497b8ba0c4aab846d4dd04a291cd17cfdb9a5c8
jabirshariff12@gmail.com <you@example.com>
2023-08-02 14:30:30 +0530

    Added components, service package and rest countries api implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import CountryCard from './Components/CountryCard';
+import {useEffect, useState} from 'react';
+import { getAllCountries } from './Services';
 
 function App() {
+  const [countriesList, setCountriesList] = useState([]);
+  useEffect(() => {
+    getAllCountries().then((result) => {
+      const countries = result.data;
+      setCountriesList(countries);
+    });
+}, []);
+
   return (
-    <div className="App">
-     <p className="app_header">Countries Information!!</p>
+    <div className="">
+      <div className="country-card-wrapper">
+        {
+        countriesList.map(country => (
+      <CountryCard
+      name={country.name}
+      capital={country.capital}
+      population={country.population}
+      flagUrl={country.flags.png}
+      key={country.alpha3Code}
+      />
+))
+}
+      </div>
     </div>
   );
 }
